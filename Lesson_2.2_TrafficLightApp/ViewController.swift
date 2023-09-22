0b57390227cc93cc0ee3f031074d8f4067863986
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2023-09-22 12:06:32 +0300

    Initial Commit

---
+++ Lesson_2.2_TrafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2.2_TrafficLightApp
+//
+//  Created by Chorrs on 22.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
