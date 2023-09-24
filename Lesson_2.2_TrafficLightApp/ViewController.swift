44bda8b3c11776f038b3cc157147360d50e397c2
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2023-09-24 19:03:50 +0300

    Lesson_2_Homework_2_ver.1

--- Lesson_2.2_TrafficLightApp/ViewController.swift
+++ Lesson_2.2_TrafficLightApp/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackviewOfLights: UIStackView!
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowlLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var buttonStart: UIButton!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonStart.layer.cornerRadius = 15
+            
+        redLight.layer.cornerRadius = 40
+        yellowlLight.layer.cornerRadius = 40
+        greenLight.layer.cornerRadius = 40
+        
+        redLight.alpha = 0.5
+        yellowlLight.alpha = 0.5
+        greenLight.alpha = 0.5
+        
+        buttonStart.setTitle("START", for: .normal)
+        
+        }
+    
+    
+        @IBAction func pressedStartButton() {
+            buttonStart.setTitle("NEXT", for: .normal)
+            
+            if redLight.alpha == 0.5 && yellowlLight.alpha == 0.5 {
+                redLight.alpha = 1
+                greenLight.alpha = 0.5
+            } else if redLight.alpha == 1 {
+                redLight.alpha = 0.5
+                yellowlLight.alpha = 1
+            } else {
+                yellowlLight.alpha = 0.5
+                greenLight.alpha = 1
+            }
+            
+        }
+        
+      
     }
-
-
-}
+    
 
